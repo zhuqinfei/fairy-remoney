e0d429e0fd1a3e7a8cb7288a98be38f45264f081
zhuqinfei <1586859762@qq.com>
2022-06-09 19:46:21 +0800

    修改仓库

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zhuqinfei/remoney-website.git master:gh-pages
+git push -f git@github.com:zhuqinfei/fairy-remoney.git master:gh-pages
 
 cd -
